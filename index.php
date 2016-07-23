48676e3decf7fa89137c8b8ef9f3944fd0e10291
Tiago Murakami <trmurakami@gmail.com>
2016-07-22 22:10:21 -0300

    Initial release

---
+++ index.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BDPI USP - Memória documental da produção científica, técnica e artística gerada nas Unidades da Universidade de São Paulo</title>
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="inc/uikit/css/uikit.min.css">
+        <link rel="stylesheet" href="inc/css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>        
+        <script src="inc/uikit/js/uikit.min.js"></script>
+        <script src="inc/uikit/js/components/grid.js"></script>
+    </head>
+
+    <body>
+        
+        <div class="barrausp">
+            <div class="uk-container uk-container-center">
+
+            <nav class="uk-margin-top">
+                <a class="uk-navbar-brand uk-hidden-small" href="index.php" style="color:white">BDPI USP</a>
+                <ul class="uk-navbar-nav uk-hidden-small">
+                    <li>
+                        <a href="index.php">Início</a>
+                    </li>
+                    <li>
+                        <a href="#" data-uk-toggle="{target:'#busca_avancada'}">Busca avançada</a>
+                    </li>
+                    <div class="uk-navbar-flip">
+                        <ul class="uk-navbar-nav">
+                            <li data-uk-dropdown="{mode:'click'}">
+                                <a href="">
+                                    Idioma
+                                    <i class="uk-icon-caret-down"></i>
+                                </a>
+                                <div class="uk-dropdown uk-dropdown-small">
+                                    <ul class="uk-nav uk-nav-dropdown">
+                                        <li><a href="">Português</a></li>
+                                        <li><a href="">Inglês</a></li>
+                                    </ul>
+                                </div> 
+                            </li>
+                            <li>
+                                <a href="contact.php">Contato</a>
+                            </li>
+                            <li>
+                                <a href="login.php">Login</a>
+                            </li>
+                            <li>
+                                <a href="about.php">Sobre</a>
+                            </li>
+                        </ul>
+                    </div>    
+                </ul>
+                <a href="#offcanvas" class="uk-navbar-toggle uk-visible-small" data-uk-offcanvas></a>
+                <div class="uk-navbar-brand uk-navbar-center uk-visible-small">BDPI USP</div>
+            </nav>
+                
+            </div>
+        </div>
+        
+        <div class="uk-container uk-container-center uk-margin-large-bottom">
+
+             <div id="busca_avancada" class="uk-grid uk-hidden" data-uk-grid-margin>
+                <div class="uk-width-medium-1-1">
+                    <p>Teste</p>
+                </div>
+            </div>
+            
+            <div class="uk-grid" data-uk-grid-margin>
+                <div class="uk-width-medium-1-1">
+                    <div class="uk-vertical-align uk-text-center" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 450px;">
+                        <div class="uk-vertical-align-middle uk-width-1-2">
+                            <h1 class="uk-heading-large">Base de Produção Intelectual da USP</h1>
+                            <p class="uk-text-large">Memória documental da produção científica, técnica e artística gerada nas Unidades da Universidade de São Paulo.</p>
+                            <form class="uk-form">
+                                <fieldset data-uk-margin>
+                                    <legend>Pesquisa</legend>
+                                    <input type="text" placeholder="Pesquise por termo ou autor" class="uk-form-width-medium">                                        
+                                    <select>
+                                        <option>Todas as bases</option>
+                                        <option>Produção científica</option>
+                                        <option>Teses e dissertações</option>
+                                    </select>
+                                    <button class="uk-button">Buscar</button>                                        
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <hr class="uk-grid-divider">
+            
+            <div class="uk-grid" data-uk-grid-margin>
+                <div class="uk-width-medium-1-3">
+                    <div class="uk-grid">
+                        <div class="uk-width-1-6">
+                            <i class="uk-icon-university uk-icon-large uk-text-primary"></i>
+                        </div>
+                        <div class="uk-width-5-6">
+                            <h2 class="uk-h3">Unidades USP</h2>
+                            <ul class="uk-list uk-list-striped">
+                                <li><a href="">Faculdade de Medicina (58989)</a></li>
+                                <li><a href="">Escola de Comunicações e Artes (58989)</a></li>
+                            </ul>
+                            <button class="uk-button" data-uk-toggle="{target:'#unidades'}">Ver todas as unidades</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-width-medium-1-3">
+                    <div class="uk-grid">
+                        <div class="uk-width-1-6">
+                            <i class="uk-icon-file uk-icon-large uk-text-primary"></i>
+                        </div>
+                        <div class="uk-width-5-6">
+                            <h2 class="uk-h3">Base</h2>
+                            <ul class="uk-list uk-list-striped">
+                                <li><a href="">Produção científica (58989)</a></li>
+                                <li><a href="">Teses e dissertações (58989)</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-width-medium-1-3">
+                    <div class="uk-grid">
+                        <div class="uk-width-1-6">
+                            <i class="uk-icon-bar-chart uk-icon-large uk-text-primary"></i>
+                        </div>
+                        <div class="uk-width-5-6">
+                            <h2 class="uk-h3">Nossos números</h2>
+                            <ul class="uk-list uk-list-striped">
+                                <li><a href="">58989 registros</a></li>
+                                <li><a href="">56565656 autores USP</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+
+            <div id="unidades" class="uk-grid uk-hidden" data-uk-grid-margin>
+                <div class="uk-width-medium-1-1">
+
+                            <ul id="filter" class="uk-subnav uk-subnav-pill">
+                                <li class="uk-active" data-uk-filter=""><a href="">Todas</a></li>
+                                <li data-uk-filter="filter-a" class=""><a href="">Humanas</a></li>
+                                <li data-uk-filter="filter-b" class=""><a href="">Exatas</a></li>
+                                <li data-uk-filter="filter-c" class=""><a href="">Biológicas</a></li>
+                            </ul>
+
+                            <div class="uk-grid-width-small-1-2 uk-grid-width-medium-1-3 uk-grid-width-large-1-4 tm-grid-heights" data-uk-grid="{controls: '#filter'}" style="position: relative; margin-left: -20px; height: 394px;">
+                                <div data-uk-filter="filter-a" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 0px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">A 1</div></div>
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 210.683px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">B 2</div></div>
+                                <div data-uk-filter="filter-a" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 421.366px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">A 3</div></div>
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 632.049px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">B 4</div></div>
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 132px; left: 0px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">B 5</div></div>
+                                <div data-uk-filter="filter-a" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 152px; left: 210.683px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">A 6</div></div>
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 212px; left: 421.366px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">B 7</div></div>
+                                <div data-uk-filter="filter-a,filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 172px; left: 632.049px; opacity: 1;" aria-hidden="false"><div class="uk-panel-box">A+B 8</div></div>
+                            </div>                
+                </div>
+            </div> 
+
+            <hr class="uk-grid-divider">
+            
+            <div class="uk-grid" data-uk-grid-margin>
+                <div class="uk-width-medium-1-1">
+                    <h2 class="uk-h3">Últimos registros</h2>
+                    <article class="uk-comment">
+                        <header class="uk-comment-header">
+                            <img class="uk-comment-avatar" src="http://getuikit.com/docs/images/placeholder_avatar.svg" alt="">
+                            <h4 class="uk-comment-title">Título mais longo talvez até muito mais longo do que deveria ser</h4>
+                            <div class="uk-comment-meta">Autor 1, Autor 2</div>
+                        </header>
+                    </article>
+                    <article class="uk-comment">
+                        <header class="uk-comment-header">
+                            <img class="uk-comment-avatar" src="http://getuikit.com/docs/images/placeholder_avatar.svg" alt="">
+                            <h4 class="uk-comment-title">Título mais longo talvez até muito mais longo do que deveria ser</h4>
+                            <div class="uk-comment-meta">Autor 1, Autor 2</div>
+                        </header>
+                    </article>                    
+                    <article class="uk-comment">
+                        <header class="uk-comment-header">
+                            <img class="uk-comment-avatar" src="http://getuikit.com/docs/images/placeholder_avatar.svg" alt="">
+                            <h4 class="uk-comment-title">Título mais longo talvez até muito mais longo do que deveria ser</h4>
+                            <div class="uk-comment-meta">Autor 1, Autor 2</div>
+                        </header>
+                    </article>                      
+                    <article class="uk-comment">
+                        <header class="uk-comment-header">
+                            <img class="uk-comment-avatar" src="http://getuikit.com/docs/images/placeholder_avatar.svg" alt="">
+                            <h4 class="uk-comment-title">Título mais longo talvez até muito mais longo do que deveria ser</h4>
+                            <div class="uk-comment-meta">Autor 1, Autor 2</div>
+                        </header>
+                    </article>  
+                    <article class="uk-comment">
+                        <header class="uk-comment-header">
+                            <img class="uk-comment-avatar" src="http://getuikit.com/docs/images/placeholder_avatar.svg" alt="">
+                            <h4 class="uk-comment-title">Título mais longo talvez até muito mais longo do que deveria ser</h4>
+                            <div class="uk-comment-meta">Autor 1, Autor 2</div>
+                        </header>
+                    </article>                      
+                </div>
+            </div>               
+            
+            <hr class="uk-grid-divider">
+            
+            <div id="footer" class="uk-grid" data-uk-grid-margin>
+                <p>Sistema Integrado de Bibliotecas da Universidade de São Paulo</p>
+            </div>
+
+        </div>
+        
+        
+
+        <div id="offcanvas" class="uk-offcanvas">
+            <div class="uk-offcanvas-bar">
+                <ul class="uk-nav uk-nav-offcanvas">
+                    <li class="uk-active">
+                        <a href="index.php">Início</a>
+                    </li>
+                    <li>
+                        <a href="#">Busca avançada</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contato</a>
+                    </li>
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="about.php">Sobre</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </body>
+</html>
