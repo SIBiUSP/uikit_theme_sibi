96af402734304a591f6ddb3a79a5cd2d6f8bd00e
Tiago Murakami <trmurakami@gmail.com>
2016-07-25 08:09:44 -0300

    Melhorias

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>BDPI USP - Memória documental da produção científica, técnica e artística gerada nas Unidades da Universidade de São Paulo</title>
         <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="inc/uikit/css/uikit.min.css">
+        <!-- <link rel="stylesheet" href="inc/uikit/css/uikit.min.css"> -->
+        <link rel="stylesheet" href="inc/uikit/css/uikit.css">
         <link rel="stylesheet" href="inc/css/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>        
         <script src="inc/uikit/js/uikit.min.js"></script>
@@ -22,33 +23,33 @@
                 <a class="uk-navbar-brand uk-hidden-small" href="index.php" style="color:white">BDPI USP</a>
                 <ul class="uk-navbar-nav uk-hidden-small">
                     <li>
-                        <a href="index.php">Início</a>
+                        <a href="index.php" style="color:white">Início</a>
                     </li>
                     <li>
-                        <a href="#" data-uk-toggle="{target:'#busca_avancada'}">Busca avançada</a>
+                        <a href="#" data-uk-toggle="{target:'#busca_avancada'}" style="color:white">Busca avançada</a>
                     </li>
                     <div class="uk-navbar-flip">
                         <ul class="uk-navbar-nav">
                             <li data-uk-dropdown="{mode:'click'}">
-                                <a href="">
+                                <a href="" style="color:white">
                                     Idioma
                                     <i class="uk-icon-caret-down"></i>
                                 </a>
                                 <div class="uk-dropdown uk-dropdown-small">
                                     <ul class="uk-nav uk-nav-dropdown">
-                                        <li><a href="">Português</a></li>
+                                        <li style="color:black"><a href="">Português</a></li>
                                         <li><a href="">Inglês</a></li>
                                     </ul>
                                 </div> 
                             </li>
                             <li>
-                                <a href="contact.php">Contato</a>
+                                <a href="contact.php" style="color:white">Contato</a>
                             </li>
                             <li>
-                                <a href="login.php">Login</a>
+                                <a href="login.php" style="color:white">Login</a>
                             </li>
                             <li>
-                                <a href="about.php">Sobre</a>
+                                <a href="about.php" style="color:white">Sobre</a>
                             </li>
                         </ul>
                     </div>    
@@ -60,7 +61,7 @@
             </div>
         </div>
         
-        <div class="uk-container uk-container-center uk-margin-large-bottom">
+
 
              <div id="busca_avancada" class="uk-grid uk-hidden" data-uk-grid-margin>
                 <div class="uk-width-medium-1-1">
@@ -70,7 +71,7 @@
             
             <div class="uk-grid" data-uk-grid-margin>
                 <div class="uk-width-medium-1-1">
-                    <div class="uk-vertical-align uk-text-center" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 450px;">
+                    <div class="uk-vertical-align uk-text-center" style="background: url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNi4wLjQsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkViZW5lXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iMTEzMHB4IiBoZWlnaHQ9IjQ1MHB4IiB2aWV3Qm94PSIwIDAgMTEzMCA0NTAiIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDExMzAgNDUwIiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCjxyZWN0IGZpbGw9IiNGNUY1RjUiIHdpZHRoPSIxMTMwIiBoZWlnaHQ9IjQ1MCIvPg0KPC9zdmc+DQo=') 50% 0 no-repeat; height: 400px;">
                         <div class="uk-vertical-align-middle uk-width-1-2">
                             <h1 class="uk-heading-large">Base de Produção Intelectual da USP</h1>
                             <p class="uk-text-large">Memória documental da produção científica, técnica e artística gerada nas Unidades da Universidade de São Paulo.</p>
@@ -90,7 +91,9 @@
                     </div>
                 </div>
             </div>
-            
+
+                <div class="uk-container uk-container-center uk-margin-large-bottom">
+        
             <hr class="uk-grid-divider">
             
             <div class="uk-grid" data-uk-grid-margin>
@@ -139,25 +142,81 @@
                 </div>                
             </div>
 
-            <div id="unidades" class="uk-grid uk-hidden" data-uk-grid-margin>
+            <div id="unidades" class="uk-grid" data-uk-grid-margin>
                 <div class="uk-width-medium-1-1">
-
+                            <h2 class="uk-h3">Todas as Unidades USP</h2>
                             <ul id="filter" class="uk-subnav uk-subnav-pill">
                                 <li class="uk-active" data-uk-filter=""><a href="">Todas</a></li>
-                                <li data-uk-filter="filter-a" class=""><a href="">Humanas</a></li>
-                                <li data-uk-filter="filter-b" class=""><a href="">Exatas</a></li>
-                                <li data-uk-filter="filter-c" class=""><a href="">Biológicas</a></li>
+                                <li data-uk-filter="filter-h" class=""><a href="">Humanas</a></li>
+                                <li data-uk-filter="filter-e" class=""><a href="">Exatas</a></li>
+                                <li data-uk-filter="filter-b" class=""><a href="">Biológicas</a></li>
                             </ul>
 
-                            <div class="uk-grid-width-small-1-2 uk-grid-width-medium-1-3 uk-grid-width-large-1-4 tm-grid-heights" data-uk-grid="{controls: '#filter'}" style="position: relative; margin-left: -20px; height: 394px;">
-                                <div data-uk-filter="filter-a" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 0px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">A 1</div></div>
-                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 210.683px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">B 2</div></div>
-                                <div data-uk-filter="filter-a" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 421.366px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">A 3</div></div>
-                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 632.049px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">B 4</div></div>
-                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 132px; left: 0px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">B 5</div></div>
-                                <div data-uk-filter="filter-a" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 152px; left: 210.683px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">A 6</div></div>
-                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 212px; left: 421.366px; opacity: 1; display: block;" aria-hidden="false"><div class="uk-panel-box">B 7</div></div>
-                                <div data-uk-filter="filter-a,filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 172px; left: 632.049px; opacity: 1;" aria-hidden="false"><div class="uk-panel-box">A+B 8</div></div>
+                            <div class="uk-grid-width-small-1-2 uk-grid-width-medium-1-3 uk-grid-width-large-1-5 tm-grid-heights" data-uk-grid="{controls: '#filter'}" style="position: relative; margin-left: -20px; height: 394px;">
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 0px; opacity: 1; display: block;" aria-hidden="false">
+                                    <div class="uk-panel uk-panel-box">
+                                        <div class="uk-panel-teaser">
+                                            <img src="http://bdpife3.sibi.usp.br/inc/images/logosusp/CEBIMAR.jpg" alt="CEBIMAR">
+                                        </div>
+                                        <p>Centro de Biologia Marinha (CEBIMAR)</p>
+                                    </div>
+                                </div>
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 210.683px; opacity: 1; display: block;" aria-hidden="false">
+                                    <div class="uk-panel uk-panel-box">
+                                        <div class="uk-panel-teaser">
+                                            <img src="http://bdpife3.sibi.usp.br/inc/images/logosusp/CENA.jpg" alt="CENA">
+                                        </div>
+                                        <p>Centro de Energia Nuclear na Agricultura (CENA)</p>
+                                    </div>                                    
+                                </div>
+                                <div data-uk-filter="filter-a" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 421.366px; opacity: 1; display: block;" aria-hidden="false">
+                                    <div class="uk-panel uk-panel-box">
+                                        <div class="uk-panel-teaser">
+                                            <img src="http://bdpife3.sibi.usp.br/inc/images/logosusp/EACH.jpg" alt="EACH">
+                                        </div>
+                                        <p>Escola de Artes, Ciências e Humanidades (EACH)</p>
+                                    </div> 
+                                </div>
+                                <div data-uk-filter="filter-h" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 0px; left: 632.049px; opacity: 1; display: block;" aria-hidden="false">
+                                    <div class="uk-panel uk-panel-box">
+                                        <div class="uk-panel-teaser">
+                                            <img src="http://bdpife3.sibi.usp.br/inc/images/logosusp/ECA.jpg" alt="ECA">
+                                        </div>
+                                        <p>Escola de Comunicações e Artes (ECA)</p>
+                                    </div>
+                                </div>
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 132px; left: 0px; opacity: 1; display: block;" aria-hidden="false">
+                                    <div class="uk-panel uk-panel-box">
+                                        <div class="uk-panel-teaser">
+                                            <img src="http://bdpife3.sibi.usp.br/inc/images/logosusp/EE.jpg" alt="EE">
+                                        </div>
+                                        <p>Escola de Enfermagem (EE)</p>
+                                    </div>                                    
+                                </div>
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 152px; left: 210.683px; opacity: 1; display: block;" aria-hidden="false">
+                                    <div class="uk-panel uk-panel-box">
+                                        <div class="uk-panel-teaser">
+                                            <img src="http://bdpife3.sibi.usp.br/inc/images/logosusp/EERP.jpg" alt="EERP">
+                                        </div>
+                                        <p>Escola de Enfermagem de Ribeirão Preto (EERP)</p>
+                                    </div> 
+                                </div>
+                                <div data-uk-filter="filter-b" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 212px; left: 421.366px; opacity: 1; display: block;" aria-hidden="false">
+                                    <div class="uk-panel uk-panel-box">
+                                        <div class="uk-panel-teaser">
+                                            <img src="http://bdpife3.sibi.usp.br/inc/images/logosusp/EEFE.jpg" alt="EEFE">
+                                        </div>
+                                        <p>Escola de Educação Física e Esporte (EEFE)</p>
+                                    </div> 
+                                </div>
+                                <div data-uk-filter="filter-e" data-grid-prepared="true" style="position: absolute; box-sizing: border-box; padding-left: 20px; padding-bottom: 20px; top: 172px; left: 632.049px; opacity: 1;" aria-hidden="false">
+                                    <div class="uk-panel uk-panel-box">
+                                        <div class="uk-panel-teaser">
+                                            <img src="http://bdpife3.sibi.usp.br/inc/images/logosusp/EEL.jpg" alt="EEL">
+                                        </div>
+                                        <p>Escola de Engenharia de Lorena (EEL)</p>
+                                    </div> 
+                                </div>
                             </div>                
                 </div>
             </div> 
